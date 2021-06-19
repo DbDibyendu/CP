791ceacb66dd3f1ac258ef46dfdac042524d532c
DbDibyendu <dbdibyendu5@gmail.com>
2021-06-19 20:43:03 +0530

    edited

---
+++ Stl and DSA/Z-algortihm.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fo(i,a,n) for(i=a;i<n;i++)
+#define ll long long
+#define deb(x) cout << #x << '=' << x << endl
+#define deb2(x, y) cout << #x << '=' << x << ',' << #y << '=' << y << endl
+#define clr(x) memset(x, 0, sizeof(x))
+#define PI 3.1415926535897932384626
+#define display(A) for(auto &aa:A) cout <<aa<<' '; cout <<endl;  
+#define displayP(A) for(auto &aa:A) {display(aa);}  
+#define read(A) for(auto &aa:A) {cin>>aa;} 
+//===========================
+typedef vector<int> vi; 
+typedef vector<ll> vl; 
+typedef pair<ll, ll> pll; 
+typedef vector<pll> vpll; 
+//=======================
+const int MOD = 1'000'000'007;
+const int N = INT_MAX, M = N;
+//=======================
+
+
+void solve() {
+ 
+    ll i, j, n, m, k;
+    ll temp=0,flag=1;
+    string str,pat;
+    cin>>str>>pat;
+    
+    m=pat.length();
+    string A=pat+"$"+str;
+
+    n=A.length();
+    // Forming the Z array;
+    vl z(n,0);
+    ll l=0,r=0;
+
+     for (i=1; i < n; ++i) {
+        if (i <= r)
+            z[i] = min(r - i + 1, z[i - l]);
+        while (i + z[i] < n && A[z[i]] == A[i + z[i]])
+            ++z[i];
+        if (i + z[i] - 1 > r)
+            l = i, r = i + z[i] - 1;
+    }
+
+    for(i=m;i<n;i++){
+        if(z[i]==m){
+            
+            // Positions of match
+            cout<<i-m-1<<endl;
+        }
+    }
+
+         
+}
+
+
+int main() {
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+    int t = 1;
+    // cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
+
+
+//=======================
